fca3f43012a94327eeb7b4a5875e14411bf46e02
Kizzi95 <kizziclaudino@gmail.com>
2025-03-28 14:09:32 -0300

    OS

---
+++ app/formulario/page.js
@@ -0,0 +1,15 @@
+function Cadastro() {
+    return (  
+        <div>
+
+            <h1> Formulário de cadastro</h1>
+            <br/>
+
+        </div>
+        
+
+
+    );
+}
+
+export default Cadastro;
