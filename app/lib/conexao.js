0c9959ab7ea6be79f8ff47d92be047c286b8d759
Hyago Souza <hyago.souza0103@gmail.com>
2025-04-08 13:57:52 -0300

    att para mexer nas api's

---
+++ app/lib/conexao.js
@@ -0,0 +1,10 @@
+import mysql from 'mysql2/promise'
+
+const conexao = await mysql.createConnection({
+    host : "localhost",
+    user : "root",
+    password : "",
+    database : "work2sell"
+});
+
+export default conexao;
