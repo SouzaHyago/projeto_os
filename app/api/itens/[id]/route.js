fa2d57a15f9e7f183255a448c068bf83f79bc2b4
KaikeAlmeida <kaikealmeida086@gmail.com>
2025-04-08 14:50:23 -0300

    routes itens & categoria

---
+++ app/api/itens/[id]/route.js
@@ -0,0 +1,32 @@
+import conexao from "@/app/lib/conexao"
+
+export async function GET(request, { params }) {
+    const id = params.id
+    const query = `SELECT * FROM itens WHERE id = ?;`
+    const [results] = await conexao.execute(query, [id])
+
+    return new Response(JSON.stringify(results[0] || {}), { status: 200 })
+}
+
+export async function PUT(request, { params }) {
+    const id = params.id
+    const body = await request.json()
+    const { nome, descricao, valor, id_categoria } = body
+
+    const query = `
+        UPDATE itens 
+        SET nome = ?, descricao = ?, valor = ?, id_categoria = ?
+        WHERE id = ?;
+    `
+    await conexao.execute(query, [nome, descricao, valor, id_categoria, id])
+
+    return new Response(JSON.stringify({ msg: "Item atualizado com sucesso!" }), { status: 200 })
+}
+
+export async function DELETE(request, { params }) {
+    const id = params.id
+    const query = `DELETE FROM itens WHERE id = ?;`
+    await conexao.execute(query, [id])
+
+    return new Response(JSON.stringify({ msg: "Item excluído com sucesso!" }), { status: 200 })
+}
