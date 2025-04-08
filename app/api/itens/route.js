4bd1472274297a2e033b4b319332b3130e59b3f1
mariaeloiza <elooiza1@gmail.com>
2025-04-08 14:43:03 -0300

    post itens e usuarios

---
+++ app/api/itens/route.js
@@ -0,0 +1,15 @@
+import conexao from "@/app/lib/conexao"
+
+export async function POST( request ){
+
+    const body = await request.json()
+    const query = `INSERT INTO itens(id_categoria, nome, descricao, valor) VALUES (?, ?, ?, ?)`
+
+    const [results] = await conexao.execute(
+        query,
+        [body.nome, body.email, body.senha, body.cpf, body.telefone]
+    )
+
+    return new Response( JSON.stringify(results.insertId) )
+
+}
