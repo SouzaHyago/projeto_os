e2b23d5d7a5a5f9b3a773e4505db562f14773b41
Hyago Souza <hyago.souza0103@gmail.com>
2025-04-17 13:50:26 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,10 @@
-// components/Menu.js
-'use client'
-import Link from 'next/link'
+import "./globals.css";
 
-export default function Menu() {
+
+export default function RootLayout({ children }) {
   return (
-    <nav style={{ padding: '10px', borderBottom: '1px solid #ccc' }}>
-      <Link href="/">Início</Link> |{' '}
-      <Link href="/categorias/new">Nova Categoria</Link> |{' '}
-      <Link href="/itens">Itens</Link> |{' '}
-      <Link href="/compras">Compras</Link>
-    </nav>
-
+    <html lang="en">
+      <body>{children}</body>
+    </html>
   )
 }
