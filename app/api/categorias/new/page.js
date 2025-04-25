73bc84e557e6a42d581ef03a88af6daa061175b1
KaikeAlmeida <kaikealmeida086@gmail.com>
2025-04-25 16:04:45 -0300

    categoria cadastro

---
+++ app/api/categorias/new/page.js
@@ -0,0 +1,45 @@
+'use client'
+import { useState } from "react"
+import axios from "axios"
+import { useRouter } from "next/navigation"
+
+export default function NovaCategoria() {
+    const [nome, setNome] = useState("")
+    const [erro, setErro] = useState(null)
+    const router = useRouter()
+
+    async function cadastrar(e) {
+        e.preventDefault()
+
+        if (!nome.trim()) {
+            setErro("O nome da categoria não pode ser vazio!")
+            return
+        }
+
+        try {
+            await axios.post("http://localhost:3000/api/categorias", { nome })
+            alert("Categoria cadastrada!")
+            router.push("/")
+        } catch {
+            setErro("Erro ao cadastrar a categoria.")
+        }
+    }
+
+    return (
+        <div>
+            <h1>Cadastrar Nova Categoria</h1>
+            {erro && <div style={{ color: "red" }}>{erro}</div>}
+            <form onSubmit={cadastrar}>
+                <input
+                    type="text"
+                    placeholder="Nome da categoria"
+                    value={nome}
+                    onChange={(e) => setNome(e.target.value)}
+                    required
+                />
+                <br />
+                <button type="submit">Cadastrar</button>
+            </form>
+        </div>
+    )
+}
