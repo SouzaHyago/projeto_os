d6b51a43a39a31e91dafa7db507c51c1aa6d8ea6
mariaeloiza <elooiza1@gmail.com>
2025-04-29 14:30:01 -0300

    Update page.js

--- app/carrinho/page.js
+++ app/carrinho/page.js
@@ -138,6 +138,7 @@ export default function Carrinho(){
                             <div className="mb-4" key={i}>
                                 <p className="text-green-900 font-medium">{item.nome}</p>
                                 <p className="text-green-800 text-sm">Quantidade: {item.qtd} • Valor: R$ {item.valor.toFixed(2)}</p>
+                                <p className="text-green-800 text-sm">Valor: R$ {item.valor.toFixed(2)}</p>
                             </div>
                         ))}
                     </div>
